4672b4796bf10296ba6297ad2b59de53423031a9
Dorian Snyder <dastin1015@gmail.com>
2012-08-08 19:26:20 -0700

    Because using Nexus 7 bootanimation

--- cm.mk
+++ cm.mk
@@ -13,8 +13,6 @@ $(call inherit-product, vendor/cm/config/wimax.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_FINGERPRINT="sprint/htc_shooter/shooter:4.0.3/IML74K/409645.2:user/release-keys" PRIVATE_BUILD_DESC="2.89.651.2 CL409645 release-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-540x960
-
 PRODUCT_NAME := cm_shooter
 PRODUCT_DEVICE := shooter
 PRODUCT_MODEL := PG86100
