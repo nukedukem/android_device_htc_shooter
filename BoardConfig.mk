48db99c82618820164b53188e76b02fbf2f2379a
Dorian Snyder <dastin1015@gmail.com>
2012-08-24 13:18:31 -0700

    I'm stupid

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ BOARD_HAVE_SQN_WIMAX := true
 BOARD_HAVE_HTC_AUDIO := true
 
 # Wifi
-WIFI_DRIVER_MODULE_NAME := bcmdhd
+WIFI_DRIVER_MODULE_NAME := bcm4329
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcm4329.ko"
 
 # cat /proc/emmc
